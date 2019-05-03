3e8bd67b9e35a800d6c2e050e79149b7441757ba
mnmjh1215 <wlghks1215@gmail.com>
2019-05-03 23:46:06 +0900

    updated config.py

--- config.py
+++ config.py
@@ -4,13 +4,19 @@ import torch
 
 class Config:
     # TODO
+    # dataloader.py
     batch_size = 128
     num_workers = 2
+    # model.py
     z_dim = 100
     gen_features = 128
-    gen_use_bias = False
+    gen_use_bias = True
     disc_features = 128
-    disc_use_bias = False
+    disc_use_bias = True
+    # train.py
+    adam_lr = 0.0002
     adam_beta1 = 0.5
+    # main.py
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     num_epochs = 20
+
