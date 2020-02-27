87fa71c0259073ad904d870b3145a77b998a35b6
mnmjh1215 <jimoon1215@gmail.com>
2020-02-27 11:13:50 +0900

    update dataloader

--- dataloader.py
+++ dataloader.py
@@ -8,96 +8,71 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
 # transform to be used for most(?) data sets
-common_transform = transforms.Compose([
-    transforms.Resize(64),  # make shorter side 64
-    transforms.CenterCrop(64),  # then center crop to make image 64 x 64
+default_transforms = transforms.Compose([
+    transforms.Resize(64),
+    transforms.CenterCrop(64),
     transforms.ToTensor(),
     transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
 ])
 
 
-def load_STL10(batch_size=128, num_workers=2):
-    """
-
-    :param batch_size: batch_size for torch.utils.data.DataLoader
-    :param num_workers: num_workers for torch.utils.data.DataLoader
-    :return: data loader for STL10 data set
-    """
+def load_STL10(batch_size=64, num_workers=2, download=False):
     # check if root directory exists
     root = 'data/STL10/'
     if not os.path.isdir(root):
-        if not os.path.isdir('data'):
-            os.mkdir('data')
-        os.mkdir(root)
+        os.makedirs(root)
 
     STL10_dataset = datasets.STL10(
         root=root,
         split="train+unlabeled",
-        transform=common_transform,
-        download=True
+        transform=default_transforms,
+        download=download
     )
 
     dataloader = DataLoader(STL10_dataset,
                             shuffle=True,
                             batch_size=batch_size,
-                            num_workers=num_workers)
+                            num_workers=num_workers,
+                            drop_last=True)
     dataloader.name = 'STL10'
 
     return dataloader
 
 
-def load_CelebA(batch_size=128, num_workers=2, download=False):
-    """
-    load CelebA data set, download if download is True and Kaggle API is available
+def load_CelebA(batch_size=64, num_workers=2, download=False):
 
-    :param batch_size: batch_size for torch.utils.data.DataLoader
-    :param num_workers: num_workers for torch.utils.data.DataLoader
-    :param download: whether to download data set. Kaggle API is required
-    :return: data loader for CelebA data set
-    """
-
-    # Couldn't figure out the best way to download data
-    # because the original link in official CelebA website is not available
     # recommend downloading from following url, or use Kaggle API.
-    # https://www.kaggle.com/jessicali9530/celeba-dataset/downloads/celeba-dataset.zip/2
+    # https://www.kaggle.com/jessicali9530/celeba-dataset/data
 
     # check if root directory exists
     root = 'data/CelebA/'
     if not os.path.isdir(root):
-        if not os.path.isdir('data'):
-            os.mkdir('data')
-        os.mkdir(root)
+        os.makedirs(root)
 
     if download and not os.path.isdir('data/CelebA/img_align_celeba'):
         import subprocess
         subprocess.run(['kaggle', 'datasets', 'download', 'jessicali9530/celeba-dataset', '-p', 'data/CelebA'])
         subprocess.run(['unzip', '-q', 'data/CelebA/celeba-dataset.zip', '-d', 'data/CelebA'])
-        subprocess.run(['unzip', '-q', 'data/CelebA/img_align_celeba.zip', '-d', 'data/CelebA'])
-        subprocess.run(['rm', 'data/CelebA/celeba-dataset.zip', 'data/CelebA/img_align_celeba.zip'])
+        subprocess.run(['mv', 'data/CelebA/img_align_celeba/img_align_celeba/*', 'data/CelebA/img_align_celeba'])
+        subprocess.run(['rm', 'data/CelebA/celeba-dataset.zip'])
+        subprocess.run(['rmdir', 'data/CelebA/img_align_celeba/img_align_celeba'])
 
     CelebA_dataset = datasets.ImageFolder(
         root=root,
-        transform=common_transform
+        transform=default_transforms
     )
 
     dataloader = DataLoader(CelebA_dataset,
                             shuffle=True,
                             batch_size=batch_size,
-                            num_workers=num_workers)
+                            num_workers=num_workers,
+                            drop_last=True)
     dataloader.name = 'CelebA'
 
     return dataloader
 
 
 def load_LSUN(batch_size=128, num_workers=2, download=False):
-    """
-    load about 10% of LSUN bedroom data set, download if download is True and Kaggle API is available
-
-    :param batch_size: batch_size for torch.utils.data.DataLoader
-    :param num_workers: num_workers for torch.utils.data.DataLoader
-    :param download: whether to download data set. Kaggle API is required
-    :return: data loader for LSUN data set
-    """
 
     # Since LSUN bedroom data is too large, we will use only 10% of them
     # https://www.kaggle.com/jhoward/lsun_bedroom
@@ -105,9 +80,7 @@ def load_LSUN(batch_size=128, num_workers=2, download=False):
     # check if root directory exists
     root = 'data/LSUN/'
     if not os.path.isdir(root):
-        if not os.path.isdir('data'):
-            os.mkdir('data')
-        os.mkdir(root)
+        os.makedirs(root)
 
     if download and not os.path.isdir('data/LSUN/sample'):
         import subprocess
@@ -118,13 +91,14 @@ def load_LSUN(batch_size=128, num_workers=2, download=False):
 
     LSUN_dataset = datasets.ImageFolder(
         root=root,
-        transform=common_transform
+        transform=default_transforms
     )
 
     dataloader = DataLoader(LSUN_dataset,
                             shuffle=True,
                             batch_size=batch_size,
-                            num_workers=num_workers)
+                            num_workers=num_workers,
+                            drop_last=True)
     dataloader.name = 'LSUN'
 
     return dataloader
